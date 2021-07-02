0e9e3d986f01fa078941be64a059a511547457fb
Cüneyt Atahan Eyüboğlu <ca.eyuboglu@gmail.com>
2021-07-03 01:30:05 +0300

    Onboarding Screen Created

---
+++ app/src/main/java/com/example/homework1/MyBasket.kt
@@ -0,0 +1,11 @@
+package com.example.homework1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyBasket : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_basket)
+    }
+}
