146491f833d8dd3a3ebcfc3c67fa5de78d00da47
Cüneyt Atahan Eyüboğlu <ca.eyuboglu@gmail.com>
2021-07-04 13:27:58 +0300

    Styling issues fix

--- app/src/main/java/com/example/homework1/MyBasket.kt
+++ app/src/main/java/com/example/homework1/MyBasket.kt
@@ -1,11 +1,19 @@
 package com.example.homework1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_my_basket.*
 
 class MyBasket : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_basket)
+
+        go_back_btn.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
