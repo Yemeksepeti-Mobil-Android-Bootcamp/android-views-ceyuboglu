627bded575c7acc2cfc9687855b2a0060c45a995
Cüneyt Atahan Eyüboğlu <ca.eyuboglu@gmail.com>
2021-07-04 03:07:25 +0300

    Basket screen created

--- app/src/main/java/com/example/homework1/MainActivity.kt
+++ app/src/main/java/com/example/homework1/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.homework1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tosecondactivity.setOnClickListener{
+            val intent = Intent(this, MyBasket::class.java)
+            // start your next activity
+            startActivity(intent)
+        }
     }
 }
